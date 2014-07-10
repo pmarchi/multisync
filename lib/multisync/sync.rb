20253820ccd282bc9db0d21576eb25a88be37940
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2014-07-10 20:49:36 +0200

    . run return executed syncs (with results)

--- lib/multisync/sync.rb
+++ lib/multisync/sync.rb
@@ -1,9 +1,14 @@
 
 class Multisync::Sync < Multisync::Entity
+  # The result from the last run
+  attr_reader :result
+  
   def run runtime, sets
-    if run? sets
-      runtime.rsync source, destination, rsync_options
-    end
+    return unless run? sets
+    puts
+    puts description.bold.yellow
+    @result = runtime.rsync source, destination, rsync_options
+    self
   end
   
   def run? sets
