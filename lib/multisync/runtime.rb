03b3cdda1c07107267d9cb7574a1caa23613062c
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2014-07-10 20:49:19 +0200

    . run actual command

--- lib/multisync/runtime.rb
+++ lib/multisync/runtime.rb
@@ -2,15 +2,24 @@
 require 'shell_cmd'
 
 class Multisync::Runtime
+  attr_reader :options
   
   def initialize options
     @options = options
   end
   
-  def rsync src, dest, options=[]
-    cmd = ['rsync', '--stats', '--verbose', options, src, dest].flatten.map do |part|
+  def rsync src, dest, rsync_options=[]
+    cmd = ['rsync', '--stats', '--verbose', rsync_options, src, dest].flatten.map do |part|
       part.gsub(/\s+/, '\\ ')
     end
-    ShellCmd.new cmd
+    ShellCmd.new(cmd).tap do |c|
+      if options[:dryrun]
+        puts c.cmd
+      else
+        c.execute do |stdout, stderr|
+          print (stdout) ? stdout : stderr
+        end
+      end
+    end
   end
 end
