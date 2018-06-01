f715921e6ca45cf631f2e32895d8af84886617bb
Patrick Marchi <patrick.marchi@geo.uzh.ch>
2018-06-01 15:25:57 +0200

    . move catalog file stuff to class

--- lib/multisync/catalog.rb
+++ lib/multisync/catalog.rb
@@ -3,28 +3,38 @@ class Multisync::Catalog
   autoload :List, 'multisync/catalog/list'
   autoload :Filter, 'multisync/catalog/filter'
   
-  # top entity of catalog
-  attr_reader :top
+  # top entity of definition
+  attr_reader :definition
 
-  def self.load file
-    new.load file
+  def initialize path
+    @path = File.expand_path(path)
   end
   
-  def load file
-    @top = Multisync::Definition::Entity.new(Multisync::Definition::Null.new, '__MAIN__')
-    top.instance_eval(File.read(file))
-    self
+  def definition
+    @_definition ||= Multisync::Definition::Entity.new(Multisync::Definition::Null.new, '__MAIN__').tap do |e|
+      e.instance_eval File.read(path)
+    end
   end
   
   def list
     catalog_list = Multisync::Catalog::List.new
-    top.accept(catalog_list)
+    definition.accept(catalog_list)
     catalog_list.result
   end
   
   def filter sets
     catalog_filter = Multisync::Catalog::Filter.new sets
-    top.accept(catalog_filter)
+    definition.accept(catalog_filter)
     catalog_filter.result
+  end
+
+  def path
+    return @path if File.exist? @path
+    sample_path = File.expand_path('../../../sample/multisync.rb', __FILE__)
+    raise RuntimeError.new, "No catalog found at #{@path}. Copy sample from #{sample_path} to #{@path} and adjust to your needs."
+  end
+  
+  def self.default_catalog_path
+    '~/.multisync.rb'
   end
 end
